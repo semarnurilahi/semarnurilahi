caacb5c20b1b97ef506bbdea93ce3a7e2bf5ae9a
semarnurilahi <107522402+semarnurilahi@users.noreply.github.com>
2022-06-15 09:51:26 +0800

    Add files via upload

---
+++ umar.py
@@ -0,0 +1,27 @@
+
+def hitung (a,b,c,):
+    a = int (a)
+    b = int (b)
+    if c == '+':
+        print(a+b)
+    elif c == 'X':
+        print(a*b)
+    elif c == '/':
+        print(a/b)
+    else:
+        print ('tidak ada operator')
+
+x = input ('masukan kode pertama:')
+y = input ('maasukan kode kedua:')
+z = input ('masukan kode operator:')
+hitung (x,y,z)
+
+class jawaban:
+    def __init__(self,jawaban):
+        self.jawaban = jawaban
+
+class jawaban:
+    pass
+    ('jawaban ini benar')
+
+print('jawaban ini telah di Akses')
